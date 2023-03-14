928e3e63ae67e38a79a642ecf1bd43e1326e14c0
ZeframLou <zeframlou@gmail.com>
2023-03-14 14:15:10 -0700

    feat: add uniswapV3PoolSlot0 view function, deploy to goerli

--- src/BunniLpZapIn.sol
+++ src/BunniLpZapIn.sol
@@ -358,4 +358,38 @@ contract BunniLpZapIn is ReentrancyGuard, Multicall, SelfPermit {
             tokenIn.safeTransfer(refundRecipient, balance);
         }
     }
+
+    /// -----------------------------------------------------------------------
+    /// Uniswap support
+    /// -----------------------------------------------------------------------
+
+    /// @notice Returns the slot0 state of a Uniswap v3 pool. Used by the frontend
+    /// as part of a static multicall to help determine 0x swap amount
+    /// that optimizes the amount of liquidity added.
+    /// @param pool The Uniswap v3 pool
+    /// @return sqrtPriceX96 The current price of the pool as a sqrt(token1/token0) Q64.96 value
+    /// @return tick The current tick of the pool, i.e. according to the last tick transition that was run.
+    /// This value may not always be equal to SqrtTickMath.getTickAtSqrtRatio(sqrtPriceX96) if the price is on a tick
+    /// boundary.
+    /// @return observationIndex The index of the last oracle observation that was written,
+    /// @return observationCardinality The current maximum number of observations stored in the pool,
+    /// @return observationCardinalityNext The next maximum number of observations, to be updated when the observation.
+    /// @return feeProtocol The protocol fee for both tokens of the pool.
+    /// Encoded as two 4 bit values, where the protocol fee of token1 is shifted 4 bits and the protocol fee of token0
+    /// is the lower 4 bits. Used as the denominator of a fraction of the swap fee, e.g. 4 means 1/4th of the swap fee.
+    /// @return unlocked Whether the pool is currently locked to reentrancy
+    function uniswapV3PoolSlot0(IUniswapV3Pool pool)
+        external view
+        returns (
+            uint160 sqrtPriceX96,
+            int24 tick,
+            uint16 observationIndex,
+            uint16 observationCardinality,
+            uint16 observationCardinalityNext,
+            uint8 feeProtocol,
+            bool unlocked
+        )
+    {
+        return pool.slot0();
+    }
 }
